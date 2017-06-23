906da232943b059ab72d36086477a3dc6c7989e9
coderaiser <coderaiser@cloudcmd.io>
2017-06-23 16:15:29 +0300

    chore(debit) rm iife

--- bin/debit.js
+++ bin/debit.js
@@ -1,27 +1,25 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var http = require('http');
+var express = require('express');
+var mollify = require('mollify');
+var router = require('../lib/router');
     
-    var http        = require('http'),
-        express     = require('express'),
-        mollify     = require('mollify'),
-        router      = require('../lib/router'),
-        
-        app         = express(),
-        server      = http.createServer(app),
-        
-        PORT        = 9876,
-        IP          = '0.0.0.0',
-        DIR         = __dirname + '/../assets';
-    
-    app .use(router)
-        .use(mollify({
-            dir: DIR
-        }))
-        .use(express.static(DIR));
-    
-    server.listen(PORT, IP);
-    
-    console.log('http://%s:%d', IP, PORT);
-})();
+var app = express();
+var server = http.createServer(app);
+
+var PORT = 9876;
+var IP = '0.0.0.0';
+var DIR = __dirname + '/../assets';
+
+app .use(router)
+    .use(mollify({
+        dir: DIR
+    }))
+    .use(express.static(DIR));
+
+server.listen(PORT, IP);
+
+console.log('http://%s:%d', IP, PORT);
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
-(function() {
-    'use strict';
-    
-    var gulp        = require('gulp'),
-        mocha       = require('gulp-mocha');
-    
-    gulp.task('test', function() {
-       gulp.src('test/test.js')
-           .pipe(mocha({reporter: 'min'}))
-           .on('error', onError);
-    });
-    
-    gulp.task('default', ['test']);
-    
-    function onError(error) {
-        console.log(error.message);
-    }
-})();
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function() {
+   gulp.src('test/test.js')
+       .pipe(mocha({reporter: 'min'}))
+       .on('error', onError);
+});
+
+gulp.task('default', ['test']);
+
+function onError(error) {
+    console.log(error.message);
+}
